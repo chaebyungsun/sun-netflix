0922d4810b10a895bf898623f4d6d47c1b804b8e
chaebyungsun <rhemdgkrtod3@naver.com>
2024-09-01 05:25:32 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import AppLayout from "./layout/AppLayout";
+import Homepage from "./pages/Homepage/Homepage";
+import MovieDetailPage from "./pages/MovieDetail/MovieDetailPage";
+import MoviePage from "./pages/Moives/MoviePage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<AppLayout />}>
+        <Route index element={<Homepage />} />
+        <Route path="movies">
+          <Route index element={<MoviePage />} />{" "}
+          <Route path=":id" element={<MovieDetailPage />} />
+        </Route>
+      </Route>
+
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
+}
+
+export default App;
